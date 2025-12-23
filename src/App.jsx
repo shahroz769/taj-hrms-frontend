c8dd6ea78a03aca3413f114b5dc722391cd73317
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2025-12-24 02:55:38 +0500

    shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+
 function App() {
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +13,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
