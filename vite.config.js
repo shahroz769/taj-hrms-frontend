fb188f69e5a057fc7dbafeae5f457c78841d0d1d
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2025-12-31 19:02:44 +0500

    Add vite-plugin-pwa and related babel dependencies for PWA support.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
@@ -12,6 +13,28 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    VitePWA(),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   manifest: {
+    //     name: "Taj HRMS",
+    //     short_name: "Taj HRMS",
+    //     description: "Taj HRMS",
+    //     theme_color: "#02542D",
+    //     icons: [
+    //       {
+    //         src: "/icons/icon-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/icons/icon-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
