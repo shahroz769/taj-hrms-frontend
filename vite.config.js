cf065d8a750bc4000aadb2033863c54134876af3
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2026-01-03 04:26:15 +0500

    disable VitePWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
@@ -13,7 +13,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    VitePWA(),
+    // VitePWA(),
     // VitePWA({
     //   registerType: "autoUpdate",
     //   manifest: {
